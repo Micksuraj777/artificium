8ea54cc344a2ec775ecde37871db2633ef592631
Mick Suraj <micksuraj999@gmail.com>
2024-03-24 14:50:39 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -13,42 +13,19 @@ function App() {
 
   return (
     <div className='bg-[#131619]'>
-      <div className='bg-[#131619]'>
       <Router>
+        <Navbar />
+        <Chatbar />
+        <Orbital/>
         <Routes>
-        <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Chatbar />
-                <Orbital/>
-                <Routes>
-                  <Route path="/" element={<Artificium />} />
-                  <Route path="/chat" element={<Chat />} />
-                  <Route path="/library" element={<Library />} />
-                </Routes>
-              </>
-            }
-          />
-        <Route path="/brand" element={
-          <>
-          <Navbar/>
-          <Brand/>
-        </>} />
-        <Route path="/digital" element={
-          <>
-          <Navbar/>
-          <Digital/>
-        </>} />
-        <Route path="/social" element={
-          <>
-          <Navbar/>
-          <Social/>
-        </>} />
+          <Route path="/" element={<Artificium />} />
+          <Route path="/brand" element={<Brand />} />
+          <Route path="/digital" element={<Digital />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/library" element={<Library />} />
         </Routes>
       </Router>
-    </div>
     </div>
   )
 }
